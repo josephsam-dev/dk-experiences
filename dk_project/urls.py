1ab8f1858dd08f7f9015e40eebcc90b3c6b1e2f1
Samuel <josephsegzy@gmail.com>
2026-03-30 17:23:34 +0100

    isolate urls test

--- dk_project/urls.py
+++ dk_project/urls.py
@@ -14,21 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.http import HttpResponse
+from django.urls import path
+
+def test(request):
+    return HttpResponse("DJANGO WORKING")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    # ✅ CORE handles HOME + CONTACT
-    path('', include('core.urls')),
-
-    # ✅ OTHER APPS
-    path('events/', include('events.urls')),
-    path('travel/', include('travel.urls')),
+    path('', test),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
