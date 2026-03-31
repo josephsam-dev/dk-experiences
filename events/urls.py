6646238b00dca902cc28c5e88e61536a4de912c1
Samuel <josephsegzy@gmail.com>
2026-03-31 18:34:40 +0100

    final fix events urls conflict

--- events/urls.py
+++ events/urls.py
@@ -2,15 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('events/', views.events_page, name='events'),
-    path('event/<int:id>/', views.event_detail, name='event_detail'),
-    path('buy-ticket/<int:id>/', views.buy_ticket, name='buy_ticket'),
-=======
     path('', views.events, name='events'),
+    path('events/', views.events_page, name='events_page'),
     path('event/<int:id>/', views.event_detail, name='event_detail'),
     path('buy-ticket/<int:id>/', views.buy_ticket, name='buy_ticket'),
     path('payment-success/', views.payment_success, name='payment_success'),
     path('blog/', views.blog, name='blog'),
->>>>>>> 2ac0cf1 (fresh clean commit without secrets)
 ]
