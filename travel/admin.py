137c09886cded1e6fe8d1257ef78ca5a8dedc6c2
Samuel <josephsegzy@gmail.com>
2026-03-31 18:19:33 +0100

    final fix admin conflict

--- travel/admin.py
+++ travel/admin.py
@@ -1,17 +1,16 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from .models import TravelPackage, Booking, BlogPost, PartnershipApplication, Ticket
-=======
-from .models import TravelPackage, Booking, BlogPost, PartnershipApplication, Ticket, TicketType
->>>>>>> 2ac0cf1 (fresh clean commit without secrets)
+from .models import (
+    TravelPackage,
+    Booking,
+    BlogPost,
+    PartnershipApplication,
+    Ticket,
+    TicketType
+)
 
 admin.site.register(TravelPackage)
 admin.site.register(Booking)
 admin.site.register(BlogPost)
 admin.site.register(PartnershipApplication)
-<<<<<<< HEAD
-admin.site.register(Ticket)
-=======
 admin.site.register(Ticket)
 admin.site.register(TicketType)
->>>>>>> 2ac0cf1 (fresh clean commit without secrets)
