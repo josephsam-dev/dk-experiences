35eb119a71acfd7ab1b9932a283c37cf3cdadab2
Samuel <josephsegzy@gmail.com>
2026-03-31 21:44:10 +0100

    fix contact in core urls

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
     path('travel/', include('travel.urls')),
+    path('contact/', views.contact, name='contact'),
+    
 ]
+
+    
--- dk_project/urls.py
+++ dk_project/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from core import views  # or wherever your contact view is
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,6 +10,8 @@ urlpatterns = [
     path('', include('core.urls')),
     path('events/', include('events.urls')),
     path('travel/', include('travel.urls')),
+    
+    
 ]
 
 if settings.DEBUG:
