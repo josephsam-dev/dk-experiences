cef511d316756adfb7874d7e1bba90005ea400f7
Samuel <josephsegzy@gmail.com>
2026-03-31 18:30:49 +0100

    fix urls merge conflict

--- core/urls.py
+++ core/urls.py
@@ -1,39 +1,8 @@
-<<<<<<< HEAD
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('', views.home, name='home'),
-    path('events/', views.events, name='events'),
-
-     path("travel/", include("travel.urls")),
+    path('', include('events.urls')),
+    path('travel/', include('travel.urls')),
 ]
-    
-
-
-# ✅ MEDIA (uploads)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# 🔥 ADD THIS (THIS IS THE FIX)
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
-
-    
-=======
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('contact/', views.contact, name='contact'),
-
-    path('blog/', views.blog, name='blog'),  # ✅ ADD THIS
-    path('blog/<int:id>/', views.blog_detail, name='blog_detail'),
-]
->>>>>>> 2ac0cf1 (fresh clean commit without secrets)
