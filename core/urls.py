78087e91b25d3da6ca9ee106938950e3adc7e4ed
Samuel <josephsegzy@gmail.com>
2026-04-01 16:35:35 +0100

    fixed core routing

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
-from core import views
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('events.urls')),
-    path('travel/', include('travel.urls')),
+    path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
-    
 ]
-
-    
