7a8346a6ef0b59ddf80b15afef6372f170878daa
Samuel <josephsegzy@gmail.com>
2026-04-04 21:17:12 +0100

    add paid field migration

--- core/models.py
+++ core/models.py
@@ -6,12 +6,14 @@ class Ticket(models.Model):
     email = models.EmailField()
     phone = models.CharField(max_length=20)
 
-    ticket_data = models.TextField()  # stores selected tickets JSON/string
+    ticket_data = models.TextField()
     total_amount = models.IntegerField()
 
     reference = models.CharField(max_length=200)
 
     created_at = models.DateTimeField(auto_now_add=True)
+
+    paid = models.BooleanField(default=False)
 
     def __str__(self):
         return self.email
